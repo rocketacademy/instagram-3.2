d269f5bfde8bd0fe5b2cf3a4cd4fe10b79c4fc86
kywk23 <kelvinywk.work@outlook.com>
2023-12-07 17:08:23 +0800

    tailwind css applied

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { onChildAdded, push, ref, set } from "firebase/database";
 import { database } from "./firebase";
 import { useState, useEffect } from "react";
+import { Container } from "postcss";
 
 // Save the Firebase message folder name as a constant to avoid bugs due to misspelling
 const DB_MESSAGES_KEY = "messages";
@@ -39,25 +40,53 @@ function App() {
   // Convert messages in state to message JSX elements to render
 
   //real time refreshing/shifting
-  let messageListItems = messages.reverse().map((message) => (
-    <li key={message.key}>
-      {message.val.Timestamp}-{message.val.Comment}
-    </li>
-  ));
+
+  // let messageListItems = messages.reverse().map((message) => (
+  //   <li key={message.key}>
+  //     {message.val.Timestamp}-{message.val.Comment}
+  //   </li>
+  // ));
 
   return (
     <>
-      <div>
-        <img src={logo} className="logo" alt="Rocket logo" />
-      </div>
-      <h1>Instagram Bootcamp</h1>
-      <div className="card">
-        <form onSubmit={writeData}>
-          <input type="text" value={inputTextValue} onChange={(e) => setInputTextValue(e.target.value)} />
-          <br />
-          <input type="submit" value="submit" />
-        </form>
-        <ul>{messageListItems}</ul>
+      <div className="bg-yellow-200 dark:bg-grey-400">
+        <h1 className="text-2xl font-bold text-rose-500">Rocketgram</h1>
+        <div className="card ">
+          <form onSubmit={writeData}>
+            <input
+              className="w-half border bg-teal-100 border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline none focus:border-indigo-500"
+              type="text"
+              value={inputTextValue}
+              onChange={(e) => setInputTextValue(e.target.value)}
+            />
+            <br /> <br />
+            <input className="bg-cyan-300 hover:bg-cyan-600 rounded-lg px-4 py-1 " type="submit" value="Submit" />
+          </form>
+        </div>
+        <container>
+          <div className="flex flex-col md:flex-row">
+            <div className="flex-1">
+              <h2 className="p-1 text-lg">Time & Date</h2>
+              <ul>
+                {messages.reverse().map((message) => (
+                  <li className="p-1" key={message.key}>
+                    {message.val.Timestamp}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="flex-1">
+              <h2 className="p-1 text-lg">Comments</h2>
+              <ul>
+                {messages.map((message) => (
+                  <li className="p-1" key={message.key}>
+                    {message.val.Comment}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </container>
       </div>
     </>
   );
