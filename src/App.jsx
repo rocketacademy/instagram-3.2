e6fe784bd2d210a5a1f12f26a037a41352c91984
jasperteo <jaspertzj@outlook.sg>
2023-12-07 21:58:39 +0800

    comfortable completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,45 @@
 import logo from "/logo.png";
 import "./App.css";
-import { onChildAdded, push, ref, set } from "firebase/database";
+import { onChildAdded, push, ref, set, onValue } from "firebase/database";
 import { database } from "./firebase";
 import { useState, useEffect } from "react";
 
 // Save the Firebase message folder name as a constant to avoid bugs due to misspelling
 const DB_MESSAGES_KEY = "messages";
 
-function App() {
+export default function App() {
   const [messages, setMessages] = useState([]);
+  const [inputMessage, setInputMessage] = useState("");
+
+  const messagesRef = ref(database, DB_MESSAGES_KEY);
 
   useEffect(() => {
-    const messagesRef = ref(database, DB_MESSAGES_KEY);
     // onChildAdded will return data for every child at the reference and every subsequent new child
     onChildAdded(messagesRef, (data) => {
       // Add the subsequent child to local component state, initialising a new array to trigger re-render
-      setMessages((prevState) =>
+      setMessages((prev) =>
         // Store message key so we can use it as a key in our list items when rendering messages
-        [...prevState, { key: data.key, val: data.val() }]
+        [...prev, { key: data.key, val: data.val() }]
       );
     });
-  }, []);
+  }, []); // eslint-disable-line
 
   const writeData = () => {
-    const messageListRef = ref(database, DB_MESSAGES_KEY);
-    const newMessageRef = push(messageListRef);
-    set(newMessageRef, "abc");
+    const newMessageRef = push(messagesRef);
+    set(newMessageRef, { Timestamp: `${Date()}`, Message: inputMessage });
   };
 
   // Convert messages in state to message JSX elements to render
-  let messageListItems = messages.map((message) => (
-    <li key={message.key}>{message.val}</li>
-  ));
+  let messageListItems = messages.map((message) => {
+    return (
+      <li key={message.key}>
+        {new Date(message.val.Timestamp).toLocaleString()}
+        <br />
+        Message: {message.val.Message}
+        <br />
+      </li>
+    );
+  });
 
   return (
     <>
@@ -41,11 +49,15 @@ function App() {
       <h1>Instagram Bootcamp</h1>
       <div className="card">
         {/* TODO: Add input field and add text input as messages in Firebase */}
-        <button onClick={writeData}>Send</button>
+        <form onSubmit={(e) => (e.preventDefault(), e.target.reset())}>
+          <input
+            type="text"
+            onChange={(e) => setInputMessage(e.target.value)}
+          />{" "}
+          <button onClick={writeData}>Send</button>
+        </form>
         <ol>{messageListItems}</ol>
       </div>
     </>
   );
 }
-
-export default App;
