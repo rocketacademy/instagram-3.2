a26fe48e07177c09eea2d19e97e5a1a015e43c26
jasperteo <jaspertzj@outlook.sg>
2023-12-09 21:06:18 +0800

    more comfortable done

--- src/App.jsx
+++ src/App.jsx
@@ -19,31 +19,18 @@ const DB_MESSAGES_KEY = "messages";
 export default function App() {
   const [messages, setMessages] = useState([]);
   const [inputValue, setInputValue] = useState("");
+  const [isEditing, setIsEditing] = useState([]);
+  const [editValue, setEditValue] = useState("");
 
   const messagesRef = ref(database, DB_MESSAGES_KEY);
 
   useEffect(() => {
-    // onChildAdded will return data for every child at the reference and every subsequent new child
     onChildAdded(messagesRef, (data) =>
-      // Add the subsequent child to local component state, initialising a new array to trigger re-render
-      setMessages((prev) =>
-        // Store message key so we can use it as a key in our list items when rendering messages
-        [...prev, { key: data.key, val: data.val() }]
-      )
+      setMessages((prev) => [...prev, { key: data.key, val: data.val() }])
     );
     onChildRemoved(messagesRef, (data) =>
       setMessages((prev) => prev.filter((item) => item.key !== data.key))
     );
-    // onChildChanged(messagesRef, (data) =>
-    //   setMessages((prev) => {
-    //     const updated = [...prev];
-    //     updated[updated.findIndex((item) => item.key === data.key)] = {
-    //       key: data.key,
-    //       val: data.val(),
-    //     };
-    //     return updated;
-    //   })
-    // );
     onChildChanged(messagesRef, (data) =>
       setMessages((prev) =>
         prev.map((item) =>
@@ -52,6 +39,10 @@ export default function App() {
       )
     );
   }, []); // eslint-disable-line
+
+  useEffect(() => {
+    setIsEditing(messages.map(() => false));
+  }, [messages]);
 
   const writeData = () =>
     set(push(messagesRef), {
@@ -62,36 +53,46 @@ export default function App() {
 
   const deleteAll = () => remove(messagesRef);
 
-  const editData = (message) =>
-    update(ref(database, DB_MESSAGES_KEY + "/" + message.key), {
-      edited: `${new Date()}`,
-      message: inputValue,
-    });
+  const editData = (data, i) => {
+    setIsEditing((prev) => prev.map((item, j) => (i === j ? !item : item)));
+    if (isEditing[i]) {
+      update(ref(database, DB_MESSAGES_KEY + "/" + data.key), {
+        edited: `${new Date()}`,
+        message: editValue,
+      });
+    }
+  };
 
-  const deleteData = (message) =>
-    remove(ref(database, DB_MESSAGES_KEY + "/" + message.key));
+  const deleteData = (data) =>
+    remove(ref(database, DB_MESSAGES_KEY + "/" + data.key));
 
   // Convert messages in state to message JSX elements to render
-  const messageListItems = messages.map((message) => {
+  const messageListItems = messages.map((message, index) => {
     return (
       <li key={message.key}>
-        <div className="text-messages">{message.val.message}</div>
+        {isEditing[index] ? (
+          <input
+            type="text"
+            value={editValue || message.val.message}
+            onChange={(e) => setEditValue(e.target.value)}
+          />
+        ) : (
+          <div className="text-messages">{message.val.message}</div>
+        )}
+        <div className="edit-delete">
+          <button onClick={() => editData(message, index)}>Edit</button>
+          <button onClick={() => deleteData(message)}>Delete</button>
+        </div>
         <div className="info">
-          <div id="timestamp">
-            <span style={{ paddingRight: "1.325em" }}>Sent: </span>
-            {new Date(message.val.timestamp).toLocaleString()}
-            {message?.val?.edited && (
-              <>
-                <br />
-                <span>Edited: </span>
-                {new Date(message.val.edited).toLocaleString()}
-              </>
-            )}
-          </div>
-          <div className="edit-delete">
-            <button onClick={() => editData(message)}>Edit</button>
-            <button onClick={() => deleteData(message)}>Delete</button>
-          </div>
+          <span style={{ paddingRight: "1.325em" }}>Sent: </span>
+          {new Date(message.val.timestamp).toLocaleString()}
+          {message?.val?.edited && (
+            <>
+              <br />
+              <span>Edited: </span>
+              {new Date(message.val.edited).toLocaleString()}
+            </>
+          )}
         </div>
       </li>
     );
