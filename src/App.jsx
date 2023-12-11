dd7487d42d41145f919c1bff6c7d49381a9ed6fe
IffahA <iffah.ada@gmail.com>
2023-12-11 14:03:26 +0800

    Added form and timing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import logo from "/logo.png";
 import "./App.css";
 import { onChildAdded, push, ref, set } from "firebase/database";
-import { database } from "./firebase";
+import {
+  ref as storageRef,
+  uploadBytes,
+  getDownloadURL,
+} from "firebase/storage";
+import { database, storage } from "./firebase";
 import { useState, useEffect } from "react";
 
 // Save the Firebase message folder name as a constant to avoid bugs due to misspelling
 const DB_MESSAGES_KEY = "messages";
+//save image key
+const STORAGE_KEY = "images/";
 
 function App() {
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState([]);
+  const [fileInputFile, setFileInputFile] = useState(null);
+  const [fileInputValue, setFileInputValue] = useState("");
 
   useEffect(() => {
     const messagesRef = ref(database, DB_MESSAGES_KEY);
@@ -18,20 +27,53 @@ function App() {
       // Add the subsequent child to local component state, initialising a new array to trigger re-render
       setMessages((prevState) =>
         // Store message key so we can use it as a key in our list items when rendering messages
-        [...prevState, { key: data.key, val: data.val() }]
+        [
+          ...prevState,
+          {
+            key: data.key,
+            val: data.val(),
+          },
+        ]
       );
     });
   }, []);
 
-  const writeData = () => {
+  const writeData = (url) => {
+    console.log(url);
     const messageListRef = ref(database, DB_MESSAGES_KEY);
     const newMessageRef = push(messageListRef);
-    set(newMessageRef, newMessage);
+    set(newMessageRef, {
+      MessageText: newMessage,
+      Timestamp: new Date().toLocaleTimeString(),
+      url: url,
+    });
+    setNewMessage("");
+    setFileInputFile(null);
+    setFileInputValue("");
+  };
+
+  //function to upload images
+  const submit = (e) => {
+    e.preventDefault();
+    const fullStorageRef = storageRef(
+      storage,
+      STORAGE_KEY + fileInputFile.name
+    );
+    console.log(fullStorageRef);
+
+    uploadBytes(fullStorageRef, fileInputFile).then(() => {
+      getDownloadURL(fullStorageRef).then((url) => {
+        writeData(url);
+      });
+    });
   };
 
   // Convert messages in state to message JSX elements to render
   let messageListItems = messages.map((message) => (
-    <li key={message.key}>{message.val}</li>
+    <li key={message.key}>
+      {message.val.MessageText} {message.val.Timestamp}{" "}
+      <img src={message.val.url} alt={message.val.url} />
+    </li>
   ));
 
   return (
@@ -41,13 +83,24 @@ function App() {
       </div>
       <h1>Instagram Bootcamp</h1>
       <div className="card">
-        <input
-          type="text"
-          onChange={(e) => {
-            setNewMessage(`${new Date()}, ${e.target.value}`);
-          }}
-        />
-        <button onClick={writeData}>Send</button>
+        <form>
+          <input
+            type="text"
+            value={newMessage}
+            onChange={(e) => {
+              setNewMessage(e.target.value);
+            }}
+          />
+          <input
+            type="file"
+            value={fileInputValue}
+            onChange={(e) => {
+              setFileInputFile(e.target.files[0]);
+              setFileInputValue(e.target.value);
+            }}
+          />
+          <button onClick={submit}>Send</button>
+        </form>
         <ol>{messageListItems}</ol>
       </div>
     </>
