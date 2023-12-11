d31ba3c65bd87763acb7e750cb3f14731a40b8f4
estney <esther.pxy@gmail.com>
2023-12-11 16:04:06 +0800

    completed base of IG chat 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,69 @@
 import logo from "/logo.png";
 import "./App.css";
-import { onChildAdded, push, ref, set } from "firebase/database";
-import { database } from "./firebase";
+import {
+  onChildAdded,
+  push,
+  ref,
+  set,
+  onChildChanged,
+  onChildRemoved,
+} from "firebase/database";
+import {
+  getDownloadURL,
+  ref as storageRef,
+  uploadBytes,
+} from "firebase/storage";
+import { database, storage } from "./firebase";
 import { useState, useEffect } from "react";
 import Card from "@mui/material/Card";
+import { useTheme } from "@emotion/react";
 
 // Save the Firebase message folder name as a constant to avoid bugs due to misspelling
 const DB_MESSAGES_KEY = "messages";
+const IMAGE_FOLDER = "images";
+
 //set date
 const DATE = new Date().toString();
 
 function App() {
   const [messages, setMessages] = useState([]);
   const [userInput, setUserInput] = useState("");
+  const [userInputFile, setUserInputFile] = useState([]);
+  const [fileInputValue, setFileInputValue] = useState("");
 
   const handleInputChange = (e) => {
     setUserInput(e.target.value);
+  };
+
+  const handleImages = (e) => {
+    setUserInputFile(e.target.files[0]);
+    setFileInputValue(e.target.value);
+  };
+
+  //edit messages from firebase
+  const editMessage = (key, newText) => {
+    const messageRef = ref(database, `${DB_MESSAGES_KEY}/${key}/text`);
+    set(messageRef, newText).then(() => {
+      console.log("Message edited successfully");
+      setMessages((prevMessages) =>
+        prevMessages.map((message) =>
+          message.key === key
+            ? { ...message, val: { ...message.val, text: newText } }
+            : message
+        )
+      );
+    });
+  };
+
+  // delete messages from firebase
+  const removeMessage = (key) => {
+    const messageRef = ref(database, `${DB_MESSAGES_KEY}/${key}`);
+    set(messageRef, null).then(() => {
+      console.log("Message removed successfully");
+      setMessages((prevMessages) =>
+        prevMessages.filter((message) => message.key !== key)
+      );
+    });
   };
 
   useEffect(() => {
@@ -28,28 +76,85 @@ function App() {
         [...prevState, { key: data.key, val: data.val() }]
       );
     });
-  }, [setMessages, setUserInput]);
+
+    // shows - edit message
+    onChildChanged(messagesRef, (data) => {
+      // Update the message in local state
+      setMessages((prevMessages) =>
+        prevMessages.map((message) =>
+          message.key === data.key
+            ? { key: data.key, val: data.val() }
+            : message
+        )
+      );
+    });
+
+    // show that the message has been removed
+    onChildRemoved(messagesRef, (data) => {
+      // Remove the message from local state
+      setMessages((prevMessages) =>
+        prevMessages.filter((message) => message.key !== data.key)
+      );
+    });
+  }, []);
 
   const writeData = (e) => {
     e.preventDefault();
-    const messageListRef = ref(database, DB_MESSAGES_KEY);
-    const newMessageRef = push(messageListRef);
 
-    const messageData = {
-      text: userInput,
-      timestamp: DATE,
-    };
+    //images
+    const fileRef = storageRef(
+      storage,
+      `${IMAGE_FOLDER}/${userInputFile.name}`
+    );
 
-    set(newMessageRef, messageData);
-    setUserInput("");
+    //upload images
+    uploadBytes(fileRef, userInputFile)
+      .then(() => getDownloadURL(fileRef))
+      .then((downloadURL) => {
+        //messages
+        const messageListRef = ref(database, DB_MESSAGES_KEY);
+        const newMessageRef = push(messageListRef);
+
+        const messageData = {
+          text: userInput,
+          timestamp: DATE,
+          imageURL: downloadURL,
+        };
+
+        set(newMessageRef, messageData);
+        setUserInput("");
+        setUserInputFile("");
+        setFileInputValue("");
+        console.log("Image URL:", downloadURL);
+      })
+      .catch((error) => {
+        // Handle errors during image upload or URL retrieval
+        console.error("Error uploading image or getting URL:", error);
+      });
   };
 
   // Convert messages in state to message JSX elements to render
   let messageListItems = messages.map((message) => (
-    <Card variant="outlined">
-      <li key={message.key}>
+    <Card variant="outlined" key={message.key}>
+      <li>
         <p>{message.val.timestamp}</p>
         <p>{message.val.text}</p>
+        <img src={message.val.imageURL} alt="Uploaded" />
+        <br />
+        <button
+          type="edit"
+          onClick={() => {
+            const newText = prompt("Edit your message:");
+            if (newText !== null) {
+              editMessage(message.key, newText);
+            }
+          }}
+        >
+          Edit
+        </button>
+        <button type="delete" onClick={() => removeMessage(message.key)}>
+          Delete
+        </button>
       </li>
     </Card>
   ));
@@ -65,8 +170,12 @@ function App() {
         <form onSubmit={writeData}>
           <input type="text" value={userInput} onChange={handleInputChange} />
           <br />
+          <br />
+          <input type="file" value={fileInputValue} onChange={handleImages} />
+          <br />
+          <br />
           <button type="submit" onClick={writeData}>
-            Send
+            Submit
           </button>
         </form>
       </div>
