1f29693b1a217f1486efcecaf284965c9ea1dc5e
IffahA <iffah.ada@gmail.com>
2023-12-13 10:40:11 +0800

    Base authentication

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ import {
 } from "firebase/storage";
 import { database, storage } from "./firebase";
 import { useState, useEffect } from "react";
+import LoginSignup from "./Components/LoginSignup";
+import FirebaseForm from "./Components/FirebaseForm";
+import { auth } from "./firebase";
+import { signOut } from "firebase/auth";
 
 // Save the Firebase message folder name as a constant to avoid bugs due to misspelling
 const DB_MESSAGES_KEY = "messages";
@@ -15,66 +19,15 @@ const DB_MESSAGES_KEY = "messages";
 const STORAGE_KEY = "images/";
 
 function App() {
-  const [messages, setMessages] = useState([]);
-  const [newMessage, setNewMessage] = useState([]);
-  const [fileInputFile, setFileInputFile] = useState(null);
-  const [fileInputValue, setFileInputValue] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState({});
 
-  useEffect(() => {
-    const messagesRef = ref(database, DB_MESSAGES_KEY);
-    // onChildAdded will return data for every child at the reference and every subsequent new child
-    onChildAdded(messagesRef, (data) => {
-      // Add the subsequent child to local component state, initialising a new array to trigger re-render
-      setMessages((prevState) =>
-        // Store message key so we can use it as a key in our list items when rendering messages
-        [
-          ...prevState,
-          {
-            key: data.key,
-            val: data.val(),
-          },
-        ]
-      );
-    });
-  }, []);
-
-  const writeData = (url) => {
-    console.log(url);
-    const messageListRef = ref(database, DB_MESSAGES_KEY);
-    const newMessageRef = push(messageListRef);
-    set(newMessageRef, {
-      MessageText: newMessage,
-      Timestamp: new Date().toLocaleTimeString(),
-      url: url,
-    });
-    setNewMessage("");
-    setFileInputFile(null);
-    setFileInputValue("");
-  };
-
-  //function to upload images
-  const submit = (e) => {
-    e.preventDefault();
-    const fullStorageRef = storageRef(
-      storage,
-      STORAGE_KEY + fileInputFile.name
-    );
-    console.log(fullStorageRef);
-
-    uploadBytes(fullStorageRef, fileInputFile).then(() => {
-      getDownloadURL(fullStorageRef).then((url) => {
-        writeData(url);
-      });
+  const handleSignOut = () => {
+    signOut(auth).then(() => {
+      setIsLoggedIn(false);
+      setUser({});
     });
   };
-
-  // Convert messages in state to message JSX elements to render
-  let messageListItems = messages.map((message) => (
-    <li key={message.key}>
-      {message.val.MessageText} {message.val.Timestamp}{" "}
-      <img src={message.val.url} alt={message.val.url} />
-    </li>
-  ));
 
   return (
     <>
@@ -82,26 +35,13 @@ function App() {
         <img src={logo} className="logo" alt="Rocket logo" />
       </div>
       <h1>Instagram Bootcamp</h1>
+      {isLoggedIn ? (
+        <button onClick={handleSignOut}>Sign out</button>
+      ) : (
+        <LoginSignup setUser={setUser} setIsLoggedIn={setIsLoggedIn} />
+      )}
       <div className="card">
-        <form>
-          <input
-            type="text"
-            value={newMessage}
-            onChange={(e) => {
-              setNewMessage(e.target.value);
-            }}
-          />
-          <input
-            type="file"
-            value={fileInputValue}
-            onChange={(e) => {
-              setFileInputFile(e.target.files[0]);
-              setFileInputValue(e.target.value);
-            }}
-          />
-          <button onClick={submit}>Send</button>
-        </form>
-        <ol>{messageListItems}</ol>
+        <FirebaseForm isLoggedIn={isLoggedIn} user={user} />
       </div>
     </>
   );
--- src/firebase.jsx
+++ src/firebase.jsx
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
 
 // TODO: Replace with your app's Firebase project configuration
 const firebaseConfig = {
@@ -24,3 +25,6 @@ export const database = getDatabase(firebaseApp);
 
 // Initialize Cloud Storage and get a reference to the service
 export const storage = getStorage(firebaseApp);
+
+//initialize firebase authentication
+export const auth = getAuth(firebaseApp);
