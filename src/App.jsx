1e45c447e800b942b3d5ef9e8c699e217bf18959
jasperteo <jaspertzj@outlook.sg>
2023-12-16 02:29:50 +0800

    disabled all other edit buttons while editing, replaced LIKE icon from SVG to html webcomponent

--- src/App.jsx
+++ src/App.jsx
@@ -122,30 +122,9 @@ export default function App() {
               );
             }}>
             {message.val.likeCount}
-            <svg
-              className="with-icon_icon__MHUeb"
-              data-testid="geist-icon"
-              height="24"
-              shapeRendering="geometricPrecision"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="1.5"
-              viewBox="0 0 24 24"
-              width="24"
-              style={{
-                color: "var(--geist-foreground)",
-                "--geist-fill": "currentColor",
-                "--geist-stroke": "var(--geist-background)",
-                width: "1em",
-                height: "1em",
-                marginBottom: "-0.075em",
-              }}>
-              <path
-                d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
-                fill="var(--geist-fill)"
-              />
-            </svg>
+            <iconify-icon
+              icon="fa6-solid:heart"
+              style={{ color: "red" }}></iconify-icon>
           </button>
         ) : (
           <button
@@ -159,34 +138,22 @@ export default function App() {
               );
             }}>
             {message.val.likeCount}
-            <svg
-              className="with-icon_icon__MHUeb"
-              data-testid="geist-icon"
-              fill="none"
-              height="24"
-              shapeRendering="geometricPrecision"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="1.5"
-              viewBox="0 0 24 24"
-              width="24"
-              style={{
-                color: "var(--geist-foreground)",
-                width: "1em",
-                height: "1em",
-                marginBottom: "-0.075em",
-              }}>
-              <path
-                d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
-                fill="var(--geist-fill)"
-              />
-            </svg>
+            <iconify-icon
+              icon="fa6-regular:heart"
+              style={{ color: "red" }}></iconify-icon>
           </button>
         )}
 
         <div className="edit-delete">
-          <button onClick={() => editData(message, index)}>Edit</button>
+          <button
+            disabled={
+              isEditing.some((bool) => bool === true)
+                ? !isEditing[index]
+                : isEditing[index]
+            }
+            onClick={() => editData(message, index)}>
+            Edit
+          </button>
           <button onClick={() => deleteData(message)}>Delete</button>
         </div>
         <div>
